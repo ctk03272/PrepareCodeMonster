009b84a010e329acbbc5a639131d73070a5036f1
Taekyung Chun <taekyungchun@Taekyungs-MacBook-Pro.local>
2020-06-20 19:31:46 +0900

    06/20

--- src/com/ctk0327/ThreeSum/Main.java
+++ src/com/ctk0327/ThreeSum/Main.java
@@ -10,10 +10,24 @@ public class Main {
 
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        Map<Integer, ArrayLi> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(-nums[i], i);
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], i);
+            }
         }
-
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+                if (map.containsKey(-sum) && map.get(-sum) != i && map.get(-sum) != j) {
+                    List<Integer> ar = new ArrayList<>();
+                    ar.add(nums[i]);
+                    ar.add(nums[j]);
+                    ar.add(-sum);
+                    answer.add(ar);
+                }
+            }
+        }
+        return answer;
     }
 }
